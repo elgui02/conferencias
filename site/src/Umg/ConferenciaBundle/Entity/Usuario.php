ad40571a301c527c1559ee5c725e57d33a61cfe3
Guillermo Arana <garana@solucionestip.com>
2015-04-28 01:38:14 +0000

    Añadiendo soporte a usuarios y seguridad

---
+++ site/src/Umg/ConferenciaBundle/Entity/Usuario.php
@@ -0,0 +1,36 @@
+<?php
+// src/Acme/UserBundle/Entity/User.php
+
+namespace Umg\ConferenciaBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="Usuario")
+ */
+class Usuario extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="Umg\ConferenciaBundle\Entity\Grupo")
+     * @ORM\JoinTable(name="fos_user_user_group",
+     *      joinColumns={@ORM\JoinColumn(name="user_id", referencedColumnName="id")},
+     *      inverseJoinColumns={@ORM\JoinColumn(name="group_id", referencedColumnName="id")}
+     * )
+     */
+    protected $groups;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}

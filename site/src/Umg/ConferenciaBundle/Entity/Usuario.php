eede6eed8dbf873605f0f9c9ac44bad1c089ecdd
Guillermo Arana <garana@solucionestip.com>
2015-05-12 20:40:23 -0600

    Añadiendo usuariAñadiendo usuarioo

--- site/src/Umg/ConferenciaBundle/Entity/Usuario.php
+++ site/src/Umg/ConferenciaBundle/Entity/Usuario.php
@@ -20,6 +20,51 @@ class Usuario extends BaseUser
     protected $id;
 
     /**
+     * @ORM\Column(type="string", length=150, nullable=true)
+     */
+    protected $Nombre;
+
+    /**
+     * @ORM\Column(type="string", length=150, nullable=true)
+     */
+    protected $Puesto;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     */
+    protected $FechaNacimiento;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    protected $Sexo;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    protected $EstadoCivil;
+
+    /**
+     * @ORM\Column(type="string", length=45, nullable=true)
+     */
+    protected $TelCasa;
+
+    /**
+     * @ORM\Column(type="string", length=45, nullable=true)
+     */
+    protected $TelCelular;
+
+    /**
+     * @ORM\Column(type="string", length=45, nullable=true)
+     */
+    protected $Direccion;
+
+    /**
+     * @ORM\Column(type="string", length=45, nullable=true)
+     */
+    protected $Foto;
+
+    /**
      * @ORM\ManyToMany(targetEntity="Umg\ConferenciaBundle\Entity\Grupo")
      * @ORM\JoinTable(name="fos_user_user_group",
      *      joinColumns={@ORM\JoinColumn(name="user_id", referencedColumnName="id")},
